b27188c4d20757dc2699421be76a8b701614ad9c
karthik <karthikreddyjannupalli@gmail.com>
2021-12-29 16:10:11 +0530

    DB

--- app/config/config.js
+++ app/config/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     db: {
-        uri: 'mongodb://huynh:123456@ds053146.mlab.com:53146/airlinereservation',
+        uri: 'mongodb+srv://karthik:karthik10dec@maindata.qgai3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
         options: {
             user: 'huynh',
             pass: '123456'
